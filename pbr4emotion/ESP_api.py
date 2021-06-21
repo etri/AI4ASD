80f452ee937dbf75c8a503dabfeaf16fa2cd123e
byungok han <byungok.han@etri.re.kr>
2021-06-21 11:09:20 +0900

    ESP_api and ESP_predict_test.py added

---
+++ pbr4emotion/ESP_api.py
@@ -0,0 +1,36 @@
+""" 
+   * Source: ESP_api.py
+   * License: PBR License (Dual License)
+   * Modified by ByungOk Han <byungok.han@etri.re.kr>
+   * Date: 21 June 2021, ETRI
+
+"""
+
+import cv2
+
+from libFD.api import PBR_libFD_predict
+from libFD.api import PBR_libFPE_predict
+from libFER.api import PBR_libFER_predict
+
+
+def PBR_ESP_predict(cv_img):
+
+    """main function
+
+    Note: main function to predict facial expression 
+
+    """   
+    
+    face_regions = PBR_libFD_predict(cv_img)
+    facial_landmarks = PBR_libFPE_predict(cv_img)
+
+    start_x, start_y, end_x, end_y, _ = face_regions[0]
+
+    face_img = cv_img[int(start_y):int(end_y), int(start_x):int(end_x)]
+
+    ret = PBR_libFER_predict(face_img)
+
+    return ret
+
+
+
---
+++ pbr4emotion/ESP_predict_test.py
@@ -0,0 +1,30 @@
+""" 
+   * Source: ESP_predict_test.py
+   * License: PBR License (Dual License)
+   * Modified by ByungOk Han <byungok.han@etri.re.kr>
+   * Date: 21 June 2021, ETRI
+
+"""
+
+import cv2
+from ESP_api import PBR_ESP_predict
+
+label_names = ['Surprise', 'Fear', 'Disgust', 'Happiness', 'Sadness', 'Anger', 'Neutral']
+
+
+def main():
+
+    """main function
+
+    Note: main function for a facial expression recognition task
+
+    """
+    
+    cv_img = cv2.imread('./libFER/fer_test.jpg')
+
+    ret = PBR_ESP_predict(cv_img)
+    print(label_names[int(ret)])
+
+
+if __name__=='__main__':
+    main()
